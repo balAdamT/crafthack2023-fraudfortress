433314dfa02642c5292624353d3436cbc53d6cc5
bAdamT <balintadamt@gmail.com>
2023-05-12 17:36:19 +0200

    init

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# How to deploy
+
+## Dependencies
+
+- Install node (^16)
+- Install pnpm (preferably `enable corepack`)
+- Install python (^3)
+
+## Build
+
+- Under ui/crafthack: `pnpm install`
+
+## Deploy (locally)
+
+- Under ui/crafthack: `pnpm dev`
+- Under bl/server: `python server.py`
---
+++ ui/crafthack/app/page.tsx
@@ -0,0 +1,11 @@
+export default async function Home() {
+  const res = await (await fetch("http://127.0.0.1:5000/test")).json();
+  
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        {res.greetings}
+      </div>
+    </main>
+  )
+}
